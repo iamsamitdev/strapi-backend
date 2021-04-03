33851461a27a9e6dc32a159d950dc01b7231f904
Samit Koyom <samitkoyom@gmail.com>
2021-04-03 23:37:18 +0700

    update database.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('WEBSITE', 'http://localhost'), // THIS ONE
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '07dba122e9a416551ed4f880ece5ecad'),
